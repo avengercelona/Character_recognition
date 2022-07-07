5bb1c5cb98827e6a154e966634faad3733170b0f
progs2002 <pragatobhattacharjee@gmail.com>
2022-07-07 13:16:30 +0530

    use cnn lmao

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 import extract
+import numpy as np
 import matplotlib.pyplot as plt
 
 import torch 
@@ -13,12 +14,29 @@ def display(t):
     a = a.numpy()
     plt.imshow(a)
 
+
+#prepare tensors
 X, Y, X_test, Y_test = extract.get_data()
 
-train_dataset = TensorDataset(torch.Tensor(X), torch.Tensor(Y))
-train_data = DataLoader(train_dataset, batch_size=64, shuffle=True)
+X = torch.Tensor(np.array(X))
+X_test = torch.Tensor(np.array(X_test))
 
-test_dataset = TensorDataset(torch.Tensor(X_test), torch.Tensor(Y_test))
-test_data = DataLoader(test_dataset, batch_size=64, shuffle=True)
+m = X.shape[0]
 
-display(next(iter(train_data)))
+index = torch.tensor(Y, dtype=torch.int64)
+index = torch.unsqueeze(index, 1)
+Y = torch.zeros(m, 26, dtype=index.dtype).scatter(1, index, value=1)
+
+index2 = torch.tensor(Y_test, dtype=torch.int64)
+index2 = torch.unsqueeze(index2, 1)
+Y_test = torch.zeros(X_test.shape[0], 26, dtype=index2.dtype).scatter(1, index2, value=1)
+
+#prepare dataset
+
+train_dataset = TensorDataset(X, Y)
+train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+
+test_dataset = TensorDataset(X_test, Y_test)
+test_dataloader = DataLoader(test_dataset, batch_size=64, shuffle=True)
+
+display(train_dataset[0])
