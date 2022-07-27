954864c5fed32a5cbf55c99466b242f010154106
progs2002 <pragatobhattacharjee@gmail.com>
2022-07-27 21:39:31 +0530

    working 92%

--- extract.py
+++
@@ -1,8 +0,0 @@
-import emnist
-import numpy as np
-
-def get_data():
-    X, Y = emnist.extract_training_samples('letters')
-    X_test, Y_test = emnist.extract_test_samples('letters')
-
-    return X, Y- 1, X_test, Y_test - 1
